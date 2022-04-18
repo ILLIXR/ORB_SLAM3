aef1fb362c0e4f866ee2aaf48c20fe172effa41e
Henry Che <daihung.che@gmail.com>
2022-04-18 18:54:28 -0500

    Added function similar to TrackStereo

--- include/System.h
+++ include/System.h
@@ -119,7 +119,9 @@ public:
     // Input images: RGB (CV_8UC3) or grayscale (CV_8U). RGB is converted to grayscale.
     // Returns the camera pose (empty if tracking fails).
     Sophus::SE3f TrackMonocular(const cv::Mat &im, const double &timestamp, const vector<IMU::Point>& vImuMeas = vector<IMU::Point>(), string filename="");
-
+    
+    // FOR ILLIXR INTEGRATION
+    Tracking* returnTracker(const cv::Mat &imLeft, const cv::Mat &imRight, const double &timestamp, const vector<IMU::Point>& vImuMeas = vector<IMU::Point>(), string filename="");
 
     // This stops local mapping thread (map building) and performs only camera tracking.
     void ActivateLocalizationMode();
